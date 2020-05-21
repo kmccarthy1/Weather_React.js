df65186773fca5a20ac35c15a6d2fc0cb96bb889
Kevin McCarthy <km.mccarthy11@gmail.com>
2020-05-21 17:09:44 -0400

    changes to App.js

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+      
+        <a target="_blank" href="https://www.gauravghai.dev/">
+          Kevin McCarthy
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="https://www.htmlhints.com/">
+          HTML HINTS
         </a>
-      </header>
-    </div>
+      </div>
+    </React.Fragment>
   );
 }
 
